69d5632935e521d1d58a43df43ad9c4cb3f43171
rasbin <rasbingodarthapa@gmail.com>
2026-09-03 23:16:46 +0545

    update dma

--- 0003_dma.c
+++ 0003_dma.c
@@ -15,15 +15,38 @@ int main() {
   for (int i = 0; i < 5; i++) {
     printf("%d ", *ptr + i);
   }
-  free(ptr);
+
   printf("\n");
 
   int *ptr2 = calloc(5, sizeof(int)); // memory items initialized with zero
 
+  if (ptr2 == NULL) {
+    printf("Memory allocation fialed\n");
+    free(ptr);
+    return 1;
+  }
+
   for (int i = 0; i < 5; i++) {
     printf("%d ", ptr[i]);
   }
-  free(ptr);
   printf("\n");
+
+  printf("size of ptr2 before realloc: %zu\n", sizeof(ptr2));
+
+  int *temp = realloc(ptr2, 10 * sizeof(int));
+
+  if (temp == NULL) {
+    printf("Memory reallocation failed\n");
+    free(ptr2);
+    return 1;
+  }
+
+  ptr2 = temp;
+
+  printf("size of ptr2 after realloc: %zu\n", sizeof(ptr2));
+
+  free(ptr);
+  free(ptr2);
+
   return 0;
 }
