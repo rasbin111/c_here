76dbd71ad48e1177e77dd71c177def9f3374cb23
rasbin <rasbingodarthapa@gmail.com>
2026-09-24 23:42:31 +0545

    read write binary file

---
+++ 0006_file_binary_mode.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  FILE *fp;
+  struct emp {
+    char name[40];
+    int age;
+    float bs;
+  };
+  struct emp e;
+  char ch = 'Y';
+
+  fp = fopen("EMPLOYEE.DAT", "w");
+
+  while (ch == 'Y') {
+    printf("Enter name, age, salary:");
+    scanf("%s %d %f", e.name, &e.age, &e.bs);
+    fprintf(fp, "%s %d %f\n", e.name, e.age, e.bs);
+    printf("Another record:");
+    ch = getchar();
+  }
+
+  fclose(fp);
+
+  fp = fopen("EMPLOYEE.DAT", "r");
+
+  while (fscanf(fp, "%s %d %f", e.name, &e.age, &e.bs) != EOF) {
+    printf("%s %d %f\n", e.name, e.age, e.bs);
+  }
+  fclose(fp);
+  return 0;
+};
